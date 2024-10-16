1a2d501fbef00f6b2e1f2821ea1d7a2f7bdd96ca
Jayanth Narasimha <75670840+jayanthn124@users.noreply.github.com>
2024-10-16 16:09:48 +0530

    Third Commit
    Changes:
    1. Updated the calculation of the End of the Samvatsara Cycle.
    2. Added proper Comments
    3. Documented the Algorithm.

--- Algorithm/SamvatsaraNama.py
+++ Algorithm/SamvatsaraNama.py
@@ -24,27 +24,38 @@ else:
 
     # Calculate the number of years from the Ref Start Cycle 
     num_of_years = start_of_ref_cycle - year_of_samvatsara
-    print("\n Number of Years from the Start of REF Cycle (54 BCE) is = ", num_of_years)
+    print("\n Number of Years from the Start of REF Cycle (54 BCE) is = ", abs(num_of_years))
 
     # If the Year falls in the Reference Cycle of 54 BCE to 6 CE
     if year_of_samvatsara >= start_of_ref_cycle and year_of_samvatsara <= end_of_ref_cycle:
+        # Start and End of the Samvatsara Cycle:
+        print("\n The Start of the Samvatsara Cycle is: ", start_of_ref_cycle)
+        print("\n The End of the Samvatsara Cycle is: ", end_of_ref_cycle)
+        # Samvatsara Index Calculation:
         if year_of_samvatsara > 0:
             samvatsara_index = abs(num_of_years + 1) # +1, since there is no YEAR ZERO.
         else:
             samvatsara_index = abs(num_of_years)
         print("\n Samvatsara Index = ", samvatsara_index)
+        # Samvatsara Name Estimation:
         print("\n The Samvatsara that falls in this year is:", SamvatsaraList[samvatsara_index])
     # If the Year falls out of the Reference Cycle of 54 BCE to 6 CE
     else:
+        # Calculate the Cycle Number:
         cycle_num = int(num_of_years / SAMVATSARA_CYCLE)
         print("\n Cycle Number = ", cycle_num)
         if year_of_samvatsara < 0:
             cycle_num += 1 # +1, since there is no YEAR ZERO.
+        # Start and End of the Samvatsara Cycle:
         start_of_the_cycle = (start_of_ref_cycle - (cycle_num * SAMVATSARA_CYCLE))
         if year_of_samvatsara > 0:
             start_of_the_cycle += 1 # +1, since there is no YEAR ZERO.
         print("\n The Start of the Samvatsara Cycle is: ", start_of_the_cycle)
+        end_of_the_cycle = start_of_the_cycle + (SAMVATSARA_CYCLE - 1)
+        print("\n The End of the Samvatsara Cycle is: ", end_of_the_cycle)
+        # Samvatsara Index Calculation:
         samvatsara_index = abs(start_of_the_cycle - year_of_samvatsara)
         print("\n Samvatsara Index = ", samvatsara_index)
+        # Samvatsara Name Estimation:
         print("\n The Samvatsara that falls in this year is:", SamvatsaraList[samvatsara_index])
         
